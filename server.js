011e6a168bede5c752f3493b809dcd3d073e9221
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2016-10-24 06:36:50 -0400

    [WIP]

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+
+var app = express();
+
+app.use(express.static('public'));
+
+app.set('view engine', 'pug');
+
+app.get('/', function (req, res) {
+  res.render('index');
+});
+
+app.listen(3000, function (err) {
+  if(err){
+    return console.log('error :P', process.exit(1));
+  }
+  console.log('listening port 3000');
+});
