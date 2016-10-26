d923353f1b55b54e27603f50d46de5d7f1840a9d
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2016-10-26 08:02:28 -0400

    Automation build

---
+++ src/index.js
@@ -0,0 +1,13 @@
+var page = require('page');
+
+var main = document.getElementById('main-container');
+
+page('/', function (ctx, next) {
+  main.innerHTML = 'Home <a href="/signup">signup</a>';
+});
+
+page('/signup', function (ctx, next) {
+  main.innerHTML = '<a href="/">Home</a> Signup';
+});
+
+page();
