c4a7cf4b7d914de3200a4978c702cb60b3c83f3f
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2016-10-27 06:52:28 -0400

    added yo-yo and empty-element library, also module divide the application

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
 var page = require('page');
-
-var main = document.getElementById('main-container');
-
-page('/', function (ctx, next) {
-  main.innerHTML = 'Home <a href="/signup">signup</a>';
-});
-
-page('/signup', function (ctx, next) {
-  main.innerHTML = '<a href="/">Home</a> Signup';
-});
+require('./homepage');
+require('./signup');
 
 page();
