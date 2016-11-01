78db135bd98de29de9819335acf7300f7f78f623
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2016-11-01 07:16:33 -0400

    imporve index.scss, homepage, signup, signin and I added template a each the view to project

--- server.js
+++ server.js
@@ -7,15 +7,15 @@ app.use(express.static('public'));
 app.set('view engine', 'pug');
 
 app.get('/', function (req, res) {
-  res.render('index');
+  res.render('index',{ title: 'Platzigram' });
 });
 
 app.get('/signup', function (req, res) {
-  res.render('index');
+  res.render('index',{ title: 'Platzigram - Signup' });
 });
 
 app.get('/signin', function (req, res) {
-  res.render('index');
+  res.render('index',{ title: 'Platzigram - SignIn' });
 });
 
 app.listen(3000, function (err) {
