286ce1c2bd548da985063316d576e8bcecfe7a26
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2017-01-06 19:36:48 -0400

    I added feature to change language

--- src/translate/en-US.js
+++ src/translate/en-US.js
@@ -2,5 +2,20 @@ module.exports = {
   likes: '{ likes, plural, ' +
             '=0 { no likes }' +
             '=1 { # like }' +
-            'other { # likes }}'
+            'other { # likes }}',
+            'logout': 'Logout',
+            'englis': 'English',
+            'spanish': 'Spanish',
+            'signup.subheading': 'Signup to watch your friend picture\'s stadying at Platzi',
+            'signup.facebook': 'Signup with FACEBOOK',
+            'signup.text': 'Signup',
+            'email': 'Email',
+            'username': 'User name',
+            'fullname': 'Full name',
+            'password': 'Password',
+            'signup.call-to-action': 'Signup',
+            'signup.have-account': 'already have an account?',
+            'signin': 'Signin',
+            'signin.not-have-account': 'Don\'t have an account?',
+            'language': 'Lenguage'
 };
--- src/translate/es.js
+++ src/translate/es.js
@@ -1,3 +1,18 @@
 module.exports = {
-  likes: '{ likes, number } me gusta'
+  'likes': '{ likes, number } me gusta',
+  'logout': 'Salir',
+  'englis': 'Ingles',
+  'spanish': 'Español',
+  'signup.subheading': 'Regístrate para ver fotos de tus amigos estudiando en Platzi',
+  'signup.facebook': 'INICIAR SESIÓN CON FACEBOOK',
+  'signup.text': 'Iniciar sesion',
+  'email': 'Correo electronico',
+  'username': 'Nombre de usuario',
+  'fullname': 'Nombre completo',
+  'password': 'contraseña',
+  'signup.call-to-action': 'Registrate',
+  'signup.have-account': 'Tienes una cuenta?',
+  'signin': 'Entrar',
+  'signin.not-have-account': 'No tienes una cuenta?',
+  'language': 'Idioma'
 };
